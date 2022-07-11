d3681a00d6afeb4fba98a25b1a8bb4f3ae5e8ccf
Andrew <afreema4@live.unc.edu>
2022-07-11 14:25:04 -0400

    Option to prepend files with source name

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import argparse
 import errno
 import os
@@ -11,11 +7,12 @@ import numpy as np
 import pandas as pd
 
 
-def convert(source, dest_dir, frame_skip, video, grayscale):
+def convert(source, dest_dir, frame_skip, video, grayscale, prepend):
     input = np.loadtxt(source, dtype=int)
 
     # Make the base directory
-    base_path = os.path.join(dest_dir, source.split('/')[-1].split('.')[0])
+    pre_name = source.split('/')[-1].split('.')[0]
+    base_path = os.path.join(dest_dir, pre_name)
     try:
         os.mkdir(base_path)
     except OSError as error:
@@ -55,7 +52,7 @@ def convert(source, dest_dir, frame_skip, video, grayscale):
     df = pd.DataFrame(input, columns=['object_id', 'object_duration', 'frame','bbox_tl_x','bbox_tl_y','bbox_width','bbox_height','class'])
 
     current_frame = frame_skip
-    f = make_file(labels_path, current_frame)
+    f = make_file(labels_path, current_frame, prepend, pre_name)
     for index, row in df.iterrows():
         if row['frame'] == current_frame:
             x_center = (row['bbox_tl_x'] + row['bbox_width']/2) / 1920
@@ -69,18 +66,24 @@ def convert(source, dest_dir, frame_skip, video, grayscale):
                 ret, frame = cap.read()
                 if grayscale:
                     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                cv2.imwrite(os.path.join(images_path, f'{current_frame:08d}' + '.png'), frame)
+
+                if prepend:
+                    cv2.imwrite(os.path.join(images_path, pre_name + '_' + f'{current_frame:08d}' + '.png'), frame)
+                else:
+                    cv2.imwrite(os.path.join(images_path, f'{current_frame:08d}' + '.png'), frame)
 
             current_frame = current_frame + frame_skip
-            f = make_file(labels_path, current_frame)
+            f = make_file(labels_path, current_frame, prepend, pre_name)
 
 
     print("Finished!")
 
-def make_file(base_path, frame_num):
-    return open(os.path.join(base_path, f'{frame_num:08d}' + '.txt'), 'w')
+def make_file(base_path, frame_num, prepend, pre_name):
+    if prepend:
+        return open(os.path.join(base_path, pre_name + '_' + f'{frame_num:08d}' + '.txt'), 'w')
+    else:
+        return open(os.path.join(base_path, f'{frame_num:08d}' + '.txt'), 'w')
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', type=str, default='', help='source VIRAT object annotation file')
@@ -90,8 +93,8 @@ if __name__ == '__main__':
                                                                    'for objects with long durations')
     parser.add_argument('--video', type=str, default='', help='extract and save images from video')
     parser.add_argument('--grayscale', action='store_true', help='convert output images to grayscale')
+    parser.add_argument('--prepend', action='store_true', help='prepend filenames with source name')
 
     opt = parser.parse_args()
     convert(**vars(opt))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
